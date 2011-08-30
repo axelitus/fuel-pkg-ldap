d593b314762c1f91d5746bb8184555c7bc114120
Axel Pardemann <axelitus2005@gmail.com>
2011-08-30 14:01:30 -0500

    Started the Ldap package

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2011 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+Autoloader::add_core_namespace('Ldap');
+
+Autoloader::add_classes(array(
+	'Ldap\\Ldap'           => __DIR__.'/classes/ldap.php'
+));
+
+
+/* End of file bootstrap.php */
